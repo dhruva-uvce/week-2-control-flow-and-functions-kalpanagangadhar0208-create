ddaada348d4c01a51313608b69e706c2c9cf4c5a
kalpanagangadhar0208-create <kalpanagangadhar0208@gmail.com>
2026-04-10 21:18:53 +0530

    Update Q06.py

--- Q06.py
+++ Q06.py
@@ -12,3 +12,12 @@
 # Sample Output 2:  0! = 1
 
 # --- YOUR CODE HERE ---
+n = int(input("Enter a number: "))
+
+if n < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    print(f"{n}! = {fact}")
